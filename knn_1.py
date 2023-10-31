19c98287a126f8a9743c88ddee9a4f10750a19a9
WoodHolz <1535980162@qq.com>
2023-10-31 21:57:56 +0800

    update

--- knn_1.py
+++ knn_1.py
@@ -2,6 +2,8 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import roc_curve, auc, precision_recall_curve, average_precision_score
+from sklearn.preprocessing import label_binarize
 import matplotlib.pyplot as plt
 
 
@@ -29,14 +31,59 @@ def knn_iris():
     score = estimator.score(x_test, y_test)
     print("准确率为：\n", score)
 
-    # 可视化
+    # 将多类别标签转换为二分类标签
+    y_test_bin = label_binarize(y_test, classes=[0, 1, 2])
+    y_predict_bin = label_binarize(y_predict, classes=[0, 1, 2])
+
+    # 计算ROC曲线
+    fpr, tpr, _ = roc_curve(y_test_bin.ravel(), y_predict_bin.ravel())
+    roc_auc = auc(fpr, tpr)
+
+    # 计算PR曲线
+    precision, recall, _ = precision_recall_curve(y_test_bin.ravel(), y_predict_bin.ravel())
+    average_precision = average_precision_score(y_test_bin, y_predict_bin, average='micro')
+
+    # 绘制散点图、PR曲线和ROC曲线
+    plt.figure(figsize=(15, 5))
+
+    # 散点图
+    plt.subplot(1, 3, 1)
     plt.scatter(x_test[:, 0], x_test[:, 1], c=y_predict)
+    # \ , label='Prediction') # 怎么尝试加入名字?????????  (*/ω＼*)
+    # plt.legend()
+
+    plt.legend()
     plt.xlabel('Sepal Length')
     plt.ylabel('Sepal Width')
     plt.title('KNN Iris Classification')
+
+
+
+    # 绘制ROC曲线
+    plt.subplot(1, 3, 2)
+
+    plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic')
+    plt.legend(loc="lower right")
+
+    # 绘制PR曲线
+    plt.subplot(1, 3, 3)
+    plt.step(recall, precision, color='b', alpha=0.2, where='post')
+    plt.fill_between(recall, precision, step='post', alpha=0.2, color='b')
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.title('Precision-Recall curve: AP={0:0.2f}'.format(average_precision))
+
+    plt.tight_layout()
     plt.show()
 
     return None
-
 
 knn_iris()
